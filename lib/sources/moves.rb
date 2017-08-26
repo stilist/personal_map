71ad2d09398bf4e2536261ce94dd73dc1783d82a
Jordan Cole <stilist@ratafia.info>
2017-08-26 09:19:24 -0700

    Add start & end time to Moves data when possible

--- lib/sources/moves.rb
+++ lib/sources/moves.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'time'
 require_relative '../to_geojson_path'
 
 class Moves
@@ -19,6 +20,29 @@ class Moves
 
   private
 
+  # Some activity segments don't include +startTime+ / +endTime+, but
+  # they may have a +duration+ that can be used if the next segment
+  # *does* include +startTime+ / +endTime+.
+  #
+  # @todo +activities+ usually includes its own +startTime+ / +endTime+ that
+  #   can be used if there's not a subsequent event.
+  def parseTimes(activities, index)
+    activity = activities[index]
+
+    startTime = activity.key?('startTime') ? Time.parse(activity['startTime']) : nil
+    endTime = activity.key?('endTime') ? Time.parse(activity['endTime']) : nil
+    if !startTime.nil? && activity['duration']
+      next_activity = activities[index + 1]
+
+      if next_activity && next_activity['startTime']
+        endTime = Time.parse(next_activity['startTime'])
+        startTime = endTime - activity['duration']
+      end
+    end
+
+    [startTime, endTime].map { |t| t&.iso8601 }
+  end
+
   def process_data(data)
     json = ::JSON.parse(data)
 
@@ -34,10 +58,13 @@ class Moves
           next if activity['activity'] == 'airplane' &&
             activity['distance'] > 50_000
 
+          startTime, endTime = parseTimes(activities, index)
           {
             coordinates: [coordinates[index]].freeze,
             properties: {
               activity: activity['activity'],
+              startTime: startTime,
+              endTime: endTime,
             }.freeze,
           }.freeze
         end
