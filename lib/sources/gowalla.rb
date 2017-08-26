44841304c2026da5f0d72b05f535ecbc7514576a
Jordan Cole <stilist@ratafia.info>
2017-08-26 16:27:05 -0700

    Reformat Gowalla data

--- lib/sources/gowalla.rb
+++ lib/sources/gowalla.rb
@@ -5,10 +5,30 @@ require 'json'
 class Gowalla
   def initialize(root:, type:)
     raw = ::File.read("#{root}/gowalla.json")
+    processed = process_data(raw)
+
     @data = raw
   end
 
   def geojson
     @data
+  end
+
+  private
+
+  def process_data(data)
+    json = JSON.parse(data)
+
+    json['features'].map do |row|
+      properties = row['properties']
+
+      {
+        coordinates: row.dig('geometry', 'coordinates').push(nil),
+        name: properties.dig('place', 'name'),
+        note: properties.dig('place', 'note'),
+        endTime: properties['endTime'] + 'Z',
+        startTime: properties['startTime'] + 'Z',
+      }.freeze
+    end
   end
 end
