39c34d02c3ad2d8a5b6176b5ea4bb32bc7412cea
Jordan Cole <stilist@ratafia.info>
2017-11-21 19:22:30 -0800

    Fix eslint errors in src/render_points

--- src/render_points.js
+++ src/render_points.js
@@ -9,26 +9,26 @@ const rangeEnd = new Date('2100-01-13T00:00:00Z')
 const pointsOptions = {
   filter: feature => {
     return isInTimeRange(feature.properties.startTime,
-                         feature.properties.endTime,
-                         rangeStart,
-                         rangeEnd)
+      feature.properties.endTime,
+      rangeStart,
+      rangeEnd)
   },
-  pointToLayer(obj, coordinates) {
+  pointToLayer (obj, coordinates) {
     const mark = L.marker(coordinates)
     if (obj.properties.place.name) mark.bindPopup(obj.properties.place.name)
 
     return mark
   },
   style: {
-    clickable: true,
-  },
+    clickable: true
+  }
 }
 
 const clusterOptions = {
   disableClusteringAtZoom: 18
 }
 
-export default function renderPoints(points) {
+export default function renderPoints (points) {
   const parsedPoints = L.geoJson(points, pointsOptions)
 
   const cluster = L.markerClusterGroup(clusterOptions)
