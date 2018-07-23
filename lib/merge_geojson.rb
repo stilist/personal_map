3d61ab4f00ca365cfbbbcb51f79ac603409f5cf4
Jordan Cole <stilist@ratafia.info>
2018-07-22 20:57:16 -0700

    Sort output data

--- lib/merge_geojson.rb
+++ lib/merge_geojson.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'active_support/time'
 require 'json'
 
 class MergeGeojson
@@ -8,12 +9,18 @@ class MergeGeojson
 
     data = sources.map { |source| process_file("#{root}/#{source}.json") }.
       flatten(1)
+    sorted = data.sort_by do |record|
+      timestamp = record.dig('properties', 'startTime')
+      next(::Date.jd(0)) if timestamp.nil?
+
+      ::Time.parse(timestamp)
+    end
     out = {
       type: 'FeatureCollection',
-      features: data,
+      features: sorted,
     }.freeze
 
-    ::File.open(output_path, 'w') { |f| f.write(::JSON.generate(out)) }
+    ::File.open(output_path, 'w') { |f| f.write(::JSON.pretty_generate(out)) }
   end
 
   private
