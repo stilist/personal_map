b445d0a06824e75b68e31ea690834c15392bc144
Jordan Cole <33663+stilist@users.noreply.github.com>
2019-11-16 16:07:27 -0800

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,35 @@
 var path = require('path')
-var webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/application.js',
+  entry: [
+    'whatwg-fetch',
+    './src/application.js',
+  ],
   output: {
     filename: 'application.js',
     path: path.resolve(__dirname, 'public', 'assets'),
     pathinfo: true,
     publicPath: '/assets/',
   },
+  mode: 'development',
 
   module: {
     rules: [{
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: 'css-loader',
-      }),
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
     }, {
       test: /\.(gif|jpe?g|png)$/,
       loader: 'file-loader',
     }, {
       test: /\.json$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 1,
-        },
-      }],
+      loader: 'url-loader',
+      type: 'javascript/auto',
+      options: {
+        limit: 1,
+      },
     }],
   },
-  plugins: [
-    new ExtractTextPlugin('application.css'),
-    // @see https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-2110690
-    new webpack.ProvidePlugin({
-      'window.fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch',
-    }),
-  ],
 }
